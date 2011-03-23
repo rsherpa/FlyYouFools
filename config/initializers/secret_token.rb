d6c2b03cce24e2cca0fedd7b12e604af02653c37
Andreas <adde.falk@gmail.com>
2011-03-23 14:08:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlyYouFools::Application.config.secret_token = '884f3f39973bb2d1f2d6f2c5597b9dd3dd4909b459c44b2df47a6ef1b7289ce054daa6fe4544c35f451629b932ddb0ad8cc52c9f15d97e3e428ceccc61572a7f'
