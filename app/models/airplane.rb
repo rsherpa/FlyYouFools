851f07dbe2297c658596982898d64a89855811fd
linus <linus@SherpasRailsZerver.kthopen.kth.se>
2011-04-06 12:56:01 +0200

    till robin

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -1,2 +1,6 @@
 class Airplane < ActiveRecord::Base
+  validates :name,  :presence => true,
+                    :length => { :minimum => 2 }
+  validates :max_passengers, :presence => true
+		:validates_numericality_of
 end
--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -1,2 +1,8 @@
 class Traveler < ActiveRecord::Base
+
+  validates :name,  :presence => true,
+                    :length => { :minimum => 2 }
+
+  validates_presence_of :personal_number
+  validates_format_of :personal_number, :with => /[0-9]{6}\-[0-9]{4}/
 end
