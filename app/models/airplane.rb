9da9bca30054f7d73eb60420e3a63886263253bd
linus <linus@SherpasRailsZerver.kthopen.kth.se>
2011-04-06 14:32:43 +0200

    tog bort det som kollar om det var int i airplane.db

--- app/models/airplane.rb
+++ app/models/airplane.rb
@@ -4,5 +4,4 @@ class Airplane < ActiveRecord::Base
   validates :name,  :presence => true,
                     :length => { :minimum => 2 }
   validates :max_passengers, :presence => true
-		:validates_numericality_of
 end
