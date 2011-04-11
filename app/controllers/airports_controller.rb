86b826a2e54680094d8edabe04c933808d785941
Andreas Falk <$EMAIL>
2011-04-11 14:43:28 +0200

    Remove airport show and update index

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,23 +1,12 @@
 class AirportsController < ApplicationController
   # GET /airports
-  # GET /airports.xml
+  # GET /airports.json
   def index
     @airports = Airport.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @airports }
-    end
-  end
-
-  # GET /airports/1
-  # GET /airports/1.xml
-  def show
-    @airport = Airport.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @airport }
+      format.json  { render :json => @airports }
     end
   end
 end
